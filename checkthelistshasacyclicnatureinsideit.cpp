5e8c2feb092a17379b2fac7bb28545d48f6f0c4a
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-11-21 10:02:13 +0530

    Add files via upload

---
+++ checkthelistshasacyclicnatureinsideit.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insertAtTail(int value) {
+        Node* new_node = new Node(value);
+        if (head == NULL) {  // Linked list is empty
+            head = new_node;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+bool detectCycle(Node*head){
+
+    if(!head){
+        return false;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast && fast->next){
+
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast){
+            cout<<slow->val<<endl;
+            return true;
+        }
+    }return false;
+}
+    
+int main() {
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.insertAtTail(7);
+    ll.insertAtTail(8);
+    ll.head->next->next->next->next->next->next->next->next = ll.head->next->next;
+    cout<<detectCycle(ll.head)<<endl;
+
+    return 0;
+}
