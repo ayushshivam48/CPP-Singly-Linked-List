1a6232a2b34d28c3b199b28e7e0548d1b666b9f7
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-11-23 10:39:57 +0530

    Add files via upload

---
+++ seperateevenoroddnodes.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insertAtTail(int value) {
+        Node* new_node = new Node(value);
+        if (head == NULL) {  // Linked list is empty
+            head = new_node;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+Node* oddEvenLL(Node* &head) {
+    if (!head || !head->next) {
+        return head;
+    }
+
+    Node* oddPtr = head;
+    Node* evenPtr = head->next;
+    Node* evenHead = evenPtr;
+
+    while (evenPtr && evenPtr->next) {
+        oddPtr->next = evenPtr->next;
+        oddPtr = oddPtr->next;
+
+        evenPtr->next = oddPtr->next;
+        evenPtr = evenPtr->next;
+    }
+
+    oddPtr->next = evenHead;
+    return head;
+}
+
+    
+int main() {
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.display();
+    ll.head = oddEvenLL(ll.head);
+    ll.display();
+    
+    return 0;
+}
